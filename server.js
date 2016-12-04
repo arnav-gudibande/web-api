ddeb66f3028e97e5a6b87411b7f5331414ed5cb2
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-12-04 14:21:13 -0800

    Modified server

--- server.js
+++ server.js
@@ -80,8 +80,6 @@ app.get("/api/events/:id/songs", function (req, res) {
       matchedEvent = events[i];
     }
   }
-
-
   res.json(matchedEvent.getSongs());
 });
 
@@ -112,30 +110,12 @@ app.post("/api/events/:id/songs", function (req, res) {
 
 // PUT {{apiUrl}}/api/events/:id/songs/:id/boost
 
-app.put("/api/events", function (req, res) {
-  var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
-  var body = _.pick(req.body, "description", "completed");
-  var validAttributes = {};
+app.put("/api/events/:id/songs/:sid/boost", function (req, res) {
+  var eventID = parseInt(req.params.id, 10);
+  var songID = req.params.sid;
+  console.log(eventID);
+  console.log(songID);
 
-  if(!matchedTodo) {
-    return res.status(404).send();
-  }
-
-  if(body.hasOwnProperty("completed") && _.isBoolean(body.completed)) {
-    validAttributes.completed = body.completed;
-  } else if (body.hasOwnProperty("completed")) {
-    return res.status(400).send();
-  }
-
-  if(body.hasOwnProperty("description") && _.isString(body.description) && (body.description.trim().length > 0)) {
-    validAttributes.description = body.description;
-  } else if (body.hasOwnProperty("description")) {
-    return res.status(400).send();
-  }
-
-  _.extend(matchedTodo, validAttributes);
-  res.json(matchedTodo);
 });
 
 //GET {{apiUrl}}/api/events/:id/pop
